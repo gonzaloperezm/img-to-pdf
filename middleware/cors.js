a4f65592ce359525f3bdff66fb458fa8bb0b01aa
gonzaloperezm <perezmazasgonzalo@gmail.com>
2024-08-16 01:02:15 +0200

    CORS Edit

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,21 +1,19 @@
-import cors from 'cors'
-export const corsMiddelware = () =>cors({
+import cors from 'cors';
+
+export const corsMiddelware = () => cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
       'http://localhost:4321',
       'http://localhost:1234',
-      'null',
-      '*'
-    ]
+      'https://img-to-pdf-j9ul.onrender.com'
+    ];
 
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-})
+  },
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
